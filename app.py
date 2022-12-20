337fc9371cbbdc269501b03f6e6b19c733addf24
CybertronicEvidence <adejuwonevidence181@gmail.com>
2022-12-20 23:18:52 +0100

    new

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 import config
-import blog
+import search
 
 
 def page_not_found(e):
@@ -18,19 +18,9 @@ def index():
 
     if request.method == 'POST':
         if 'form1' in request.form:
-            prompt = request.form['blogTopic']
-            blogT = blog.generateBlogTopics(prompt)
-            blogTopicIdeas = blogT.replace('\n', '<br>')
-
-        if 'form2' in request.form:
-            prompt = request.form['blogSection']
-            blogT = blog.generateBlogSections(prompt)
-            blogSectionIdeas = blogT.replace('\n', '<br>')
-
-        if 'form3' in request.form:
-            prompt = request.form['blogExpander']
-            blogT = blog.blogSectionExpander(prompt)
-            blogExpanded = blogT.replace('\n', '<br>')
+            prompt = request.form['Search']
+            searchT = search.generateIdeas(prompt)
+            searchIdeas = searchT.replace('\n', '<br>')
 
 
     return render_template('index.html', **locals())
--- search.py
+++ search.py
@@ -18,7 +18,7 @@ openai.api_key = config.OPENAI_API_KEY
 
 # openai.api_key = get_api_key()  
 
-def generateBlogTopics(prompt1):
+def generateIdeas(prompt1):
     response = openai.Completion.create(
       engine="text-davinci-003",
       prompt="{}.".format(prompt1),
@@ -31,29 +31,3 @@ def generateBlogTopics(prompt1):
 
     return response['choices'][0]['text']
 
-def generateBlogSections(prompt1):
-    response = openai.Completion.create(
-      engine="davinci-instruct-beta-v3",
-      prompt="Expand the blog title in to high level blog sections: {} \n\n- Introduction: ".format(prompt1),
-      temperature=0.6,
-      max_tokens=100,
-      top_p=1,
-      frequency_penalty=0,
-      presence_penalty=0
-    )
-
-    return response['choices'][0]['text']
-
-
-def blogSectionExpander(prompt1):
-    response = openai.Completion.create(
-      engine="davinci-instruct-beta-v3",
-      prompt="Expand the blog section in to a detailed professional , witty and clever explanation.\n\n {}".format(prompt1),
-      temperature=0.7,
-      max_tokens=200,
-      top_p=1,
-      frequency_penalty=0,
-      presence_penalty=0
-    )
-
-    return response['choices'][0]['text']
